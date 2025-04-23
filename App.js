c790464fa69c6f5a9fc2ea3f38a08fd71e47b6ec
getGravvy <getgravvy42@gmail.com>
2025-04-23 17:21:41 -0600

    Update App.js

--- App.js
+++ App.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import AppNavigator from './AppNavigator';
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+} from '@expo-google-fonts/inter';
+import AppLoading from 'expo-app-loading';
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
     <NavigationContainer>
       <AppNavigator />
